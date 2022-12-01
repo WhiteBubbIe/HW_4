153e89a994d08734e6873356764f015992e66737
WhiteBubbIe <0kiserr@gmail.com>
2022-12-02 00:10:12 +0500

    homework_4

---
+++ main.py
@@ -0,0 +1,20 @@
+def myFunk(name_func, *args):
+    name = name_func.__name__.title().replace('_', " ")
+    print(name, *args)
+
+
+def open_browser(browser_name):
+    myFunk(open_browser, browser_name)
+
+
+def go_to_companyname_homepage(page_url):
+    myFunk(go_to_companyname_homepage, page_url)
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    myFunk(find_registration_button_on_login_page, page_url, button_text)
+
+
+open_browser("Chrome")
+go_to_companyname_homepage("https://stepik.org/")
+find_registration_button_on_login_page("https://stepik.org/", "login")
